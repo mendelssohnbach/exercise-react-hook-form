8eacbfd5b819c0d2ff1fd4c75c1aef07189d2e45
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-13 08:18:03 +0900

    演習実装 スキーマバリデーション
    https://react-hook-form.com/jp/get-started#SchemaValidation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 // https://react-hook-form.com/jp/get-started
-// エラーを処理する
+// スキーマバリデーション
 
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  age: yup.number().positive().integer().required(),
+});
 
 export default function App() {
   const {
     register,
-    formState: { errors },
     handleSubmit,
-  } = useForm();
-
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-  };
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName', { required: true })} />
-      {errors.firstName?.type === 'required' && 'First name is required'}
-
-      <input {...register('lastName', { required: true })} />
-      {errors.lastName && 'Last name is required'}
-
+      <input {...register('firstName')} />
+      <p>{errors.firstName?.message}</p>
+      <input {...register('age')} />
+      <p>{errors.age?.message}</p>
       <input type="submit" />
     </form>
   );
