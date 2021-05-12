5835bb2535ac2f596b1cf353c065285ae7e5ca68
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2021-05-12 17:40:47 +0900

    演習実装 UIライブラリを使用

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 // https://react-hook-form.com/jp/get-started
-// 既存のフォームに適用
+// UI ライブラリを使用
 
-import React from 'react';
-import { useForm } from 'react-hook-form';
-
-// The following component is an example of your existing Input Component
-const Input = ({ label, register, required }) => (
-  <>
-    <label>{label}</label>
-    <input {...register(label, { required })} />
-  </>
-);
-
-// また、React.forwardRefを使ってRefを渡すこともできます
-const Select = React.forwardRef(({ onChange, onBlur, name, label }, ref) => (
-  <>
-    <label>{label}</label>
-    <select name={name} ref={ref} onChange={onChange} onBlur={onBlur}>
-      <option value="20">20</option>
-      <option value="30">30</option>
-    </select>
-  </>
-));
+import Select from 'react-select';
+import { useForm, Controller } from 'react-hook-form';
+import Input from '@material-ui/core/Input';
 
 const App = () => {
-  const { register, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     alert(JSON.stringify(data));
@@ -32,9 +14,27 @@ const App = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Input label="First Name" register={register} required />
-      <Select label="Age" {...register('Age')} />
-      <input type="submit" />
+      <Controller
+        name="firstName"
+        control={control}
+        defaultValue=""
+        render={({ field }) => <Input {...field} />}
+      />
+      <Controller
+        name="iceCreamType"
+        control={control}
+        render={({ field }) => (
+          <Select
+            {...field}
+            options={[
+              { value: 'chocolate', label: 'Chocolate' },
+              { value: 'strawberry', label: 'Strawberry' },
+              { value: 'vanilla', label: 'Vanilla' },
+            ]}
+          />
+        )}
+      />
+      <Input type="submit" />
     </form>
   );
 };
